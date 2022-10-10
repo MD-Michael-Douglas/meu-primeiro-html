3ebc754faa996d3ddfffcea1a1b7dfa6d7760f59
Michael Douglas <Michael Douglas>
2022-10-09 23:39:20 -0300

    Commit de teste de estudo!

---
+++ app.js
@@ -0,0 +1,6 @@
+const buttonElement = document.getElementById('btn');
+
+
+buttonElement.addEventListener('click',function(event) {
+    document.getElementById("nome").innerHTML = "Meu nome é: Michael Douglas";
+})
